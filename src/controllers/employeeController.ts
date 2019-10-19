1dcbb3ddd9a4406892711644de914b2b82c9ad5a
Chintan Shah <chintan.shah12@outlook.com>
2019-10-19 09:40:36 +0530

    fixed unprepare issue

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,19 +1,21 @@
-import { Request, Response} from 'express';
-import {ConnectionPool, Int, VarChar, PreparedStatement} from 'mssql/msnodesqlv8';
+import { Request, Response } from 'express';
+import { ConnectionPool, Int, VarChar, PreparedStatement } from 'mssql/msnodesqlv8';
 import ConnectionFactory from '../utils/connectionFactory';
+import debug from 'debug';
 
-export default class EmployeeController {    
-    constructor(private readonly connectionFactory: ConnectionFactory){
-        
+export default class EmployeeController {
+    private readonly d: debug.Debugger;
+    constructor(private readonly connectionFactory: ConnectionFactory) {
+        this.d = debug("app:employee:controller");
     }
     public async getEmployees(req: Request, res: Response, next: Function) {
-        try {                    
+        try {
             const connection: ConnectionPool = await this.connectionFactory.getConnection();
             const employees = await connection.query('SELECT * FROM Employees');
             connection.close();
-            res.render('employee/employees', {employees: employees.recordset});
+            res.render('employee/employees', { employees: employees.recordset });
         } catch (ex) {
-            console.error(ex);
+            this.d(ex);
         }
     }
 
@@ -21,33 +23,39 @@ export default class EmployeeController {
         res.render('employee/addEmployee');
     }
 
-    public async addEmployee(req: Request, res: Response, next: Function) {        
-        const emp = req.body;                
+    public async addEmployee(req: Request, res: Response, next: Function) {
+        const emp = req.body;
         const connection = await this.connectionFactory.getConnection();
         const request = connection.request();
-        request.input('name',VarChar(50),emp.Name);
-        const result = await request.query('Insert INTO Employees (Name) VALUES (@name)');        
+        request.input('name', VarChar(50), emp.Name);
+        const result = await request.query('Insert INTO Employees (Name) VALUES (@name)');
         connection.close();
-        res.redirect('/employees');        
+        res.redirect('/employees');
     }
     public async getEditEmployeeView(req: Request, res: Response, next: Function) {
-        const connection = await this.connectionFactory.getConnection();
-        const ps = new PreparedStatement(connection);
-        ps.input('id', Int);
-        await ps.prepare('SELECT * FROM Employees WHERE Id = @id');
-        const empId = req.params.id;
-        const result = await ps.execute({id: empId});                        
-        await connection.close();
-        res.render('employee/editEmployee', {emp: result.recordset[0]});
+        try {
+            const connection = await this.connectionFactory.getConnection();
+            const ps = new PreparedStatement(connection);
+            ps.input('id', Int);
+            await ps.prepare('SELECT * FROM Employees WHERE Id = @id');
+            const empId = req.params.id;
+            const result = await ps.execute({ id: empId });
+            await ps.unprepare();
+            await connection.close();
+            res.render('employee/editEmployee', { emp: result.recordset[0] });
+        } catch (err) {
+            this.d(err);
+        }
+
     }
-    public async editEmployee(req: Request, res: Response, next: Function) {        
-        let emp = req.body;            
+    public async editEmployee(req: Request, res: Response, next: Function) {
+        let emp = req.body;
         const connection = await this.connectionFactory.getConnection();
         const request = connection.request();
-        request.input('name', VarChar(50),emp.Name);
-        request.input('id', Int, emp.Id);
+        request.input('name', VarChar(50), emp.Name);
+        request.input('id', Int, req.params.id);
         await request.query('UPDATE Employees SET Name = @name WHERE ID = @id');
         await connection.close();
-        res.redirect('/employees');        
+        res.redirect('/employees');
     }
 }
